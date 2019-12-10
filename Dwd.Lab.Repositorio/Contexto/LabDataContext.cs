f8d9f3c257eaad4df30c5c7f6e08b6d46840b7b6
Felipe Neves <felipeneves089@gmail.com>
2019-12-09 23:38:11 -0300

    Configuração da classe Startup.cs e config.json

---
+++ Dwd.Lab.Repositorio/Contexto/LabDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Dwd.Lab.Dominio.Entidades;
+using Dwd.Lab.Dominio.ObjetoDeValor;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dwd.Lab.Repositorio.Contexto
+{
+    public class LabDataContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<ItemPedido> ItemPedido { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+
+    }
+}
