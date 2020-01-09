485a8bfc430d15d85ca515a417b44ec449fef0ff
Felipe Neves <felipeneves089@gmail.com>
2020-01-09 00:19:19 -0300

    2020-01-09

--- Dwd.Lab.Web/ClientApp/src/app/pages/usuario/login-usuario-page/login-usuario-page.component.ts
+++ Dwd.Lab.Web/ClientApp/src/app/pages/usuario/login-usuario-page/login-usuario-page.component.ts
@@ -4,11 +4,14 @@ import { Usuario } from 'src/app/Models/usuario';
 @Component({
   selector: 'app-login-usuario-page',
   templateUrl: './login-usuario-page.component.html',
-  styleUrls: ['./login-usuario-page.component.css']
+  styleUrls: ['./login-usuario-page.component.css'],  
 })
 export class LoginUsuarioPageComponent implements OnInit {  
 
   public usuario;
+  public emailSis = "felipeneves089@gmail.com";
+  public senhaSis = "123";
+  public logado: boolean = false;
   
 
   constructor() {
@@ -19,8 +22,10 @@ export class LoginUsuarioPageComponent implements OnInit {
     
   }
 
-  loginUsuario() : void{
-    alert(this.usuario.email + " - " + this.usuario.senha);
+  loginUsuario() : void{     
+    if(this.usuario.email == this.emailSis && this.usuario.senha == this.senhaSis ){
+        this.logado = true;
+    }
   }
 
 }
