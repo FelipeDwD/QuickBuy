202e0bc70349d6fdc77669776d09a69fac5e16f8
Felipe Neves <felipeneves089@gmail.com>
2020-03-31 00:13:15 -0300

    2020-03-31 PT.

--- Dwd.Lab.Web/ClientApp/src/app/Models/usuario.ts
+++ Dwd.Lab.Web/ClientApp/src/app/Models/usuario.ts
@@ -1,11 +1,6 @@
 export class Usuario {
-    id: number;
-    nome: string;
-    sobrenome: string;
-    cpf: string;
-    email: string;
-    senha: string;
-    imagem: string;
-    sexo: string;
-    ativo: boolean;    
+    id:number;
+    email:string;
+    senha:string;
+    imagem:string; 
 }
